c700e9fdf8abd7809f8fb7f446fa0163804dc4de
= <tajikarao@protonmail.com>
2023-03-21 16:13:35 -0700

    [UPDATE] damerau_levenshtein take synonyms

--- algos/damerau_levenshtein.py
+++ algos/damerau_levenshtein.py
@@ -7,9 +7,27 @@ def damerau_lvst(anime_list, search, limit):
         limit = 10
     limit = int(limit)
 
-    scores = [
-        damerau_levenshtein_distance(search.lower(), anime.lower())
-        for anime in anime_list
-    ]
-    indices = np.argsort(scores)[:limit]
-    return [anime_list[i] for i in indices]
+    texts = []
+    labels = []
+    for anime_dict in anime_list:
+        for anime_name, variations in anime_dict.items():
+            for variation in variations:
+                texts.append(variation)
+                labels.append(anime_name)
+
+    distances = np.array([damerau_levenshtein_distance(search, text) for text in texts])
+    indices = np.argsort(distances)[:limit]
+
+    anime_distances = {}
+    for index in indices:
+        label = labels[index]
+        distance = distances[index]
+        if (
+            label in anime_distances
+            and distance < anime_distances[label]
+            or label not in anime_distances
+        ):
+            anime_distances[label] = distance
+
+    sorted_anime_distances = sorted(anime_distances.items(), key=lambda x: x[1])
+    return [anime_name for anime_name, _ in sorted_anime_distances[:limit]]
--- routes/damerau_levenshtein.py
+++ routes/damerau_levenshtein.py
@@ -9,6 +9,6 @@ blueprint = Blueprint("damerau_levenshtein")
 @blueprint.route("/damerau_levenshtein/<search>/<limit>", methods=["GET"])
 @cache.cached(timeout=20)
 def damerau_levenshtein(search, limit):
-    anime_list = anime.get_names()
+    anime_list = anime.get_names_and_synonyms()
 
     return damerau_lvst(anime_list, search, limit)
